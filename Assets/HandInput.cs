6085390f3c650520fdeecca548b56de917e9910d
Alanoudwd <121495847+Alanoudwd@users.noreply.github.com>
2024-01-28 19:36:10 +0300

    PUSH

---
+++ Assets/HandInput.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HandInput : MonoBehaviour
+{
+
+    public InputActionProperty pinch;
+    public Animator animator;
+    public InputActionProperty grip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float triggerValue = pinch.action.ReadValue<float>();
+        animator.SetFloat("Trigger", triggerValue);
+
+        float gripValue = grip.action.ReadValue<float>();
+        animator.SetFloat("Grip", gripValue);
+
+        
+    }
+}
